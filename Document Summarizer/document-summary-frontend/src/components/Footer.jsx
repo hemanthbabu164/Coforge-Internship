85d412ad2966722bb5b80fbb4760a6e3e94d4420
Hemanth Babu <hemanthbabu1131@gmail.com>
2024-08-15 01:47:32 +0530

    Added Styling

--- Document Summarizer/document-summary-frontend/src/components/Footer.jsx
+++ Document Summarizer/document-summary-frontend/src/components/Footer.jsx
@@ -3,13 +3,15 @@ import { Typography, Box, Container } from "@mui/material";
 
 function Footer() {
   return (
-    <Box component="footer" padding={2} bgcolor="#f5f5f5" textAlign="center">
-      <Container>
-        <Typography variant="body2" color="textSecondary">
-          &copy; {new Date().getFullYear()} Document Summary Generator.
-        </Typography>
-      </Container>
-    </Box>
+    <div className="footerSection">
+      <Box component="footer" padding={2} bgcolor="#f5f5f5" textAlign="center">
+        <Container>
+          <Typography variant="body2" color="textSecondary">
+            &copy; {new Date().getFullYear()} Document Summary Generator.
+          </Typography>
+        </Container>
+      </Box>
+    </div>
   );
 }
 
--- Document Summarizer/document-summary-frontend/src/components/Header.jsx
+++ Document Summarizer/document-summary-frontend/src/components/Header.jsx
@@ -3,35 +3,37 @@ import { AppBar, Toolbar, Typography, Box, Container } from "@mui/material";
 
 function Header() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        width: "100%",
-      }}
-    >
-      <AppBar
-        position="static"
+    <div className="headerSection">
+      <Box
         sx={{
-          backgroundColor: "#1976d2",
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
-          width: "auto",
-          borderRadius: "8px",
-          margin: "16px 0",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
         }}
       >
-        <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
-          <Container
-            maxWidth="md"
-            sx={{ textAlign: "center", width: "auto", padding: "0" }}
-          >
-            <Typography variant="h2" component="h2">
-              Document Summary Generator
-            </Typography>
-          </Container>
-        </Toolbar>
-      </AppBar>
-    </Box>
+        <AppBar
+          position="static"
+          sx={{
+            backgroundColor: "#1976d2",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
+            width: "auto",
+            borderRadius: "8px",
+            margin: "16px 0",
+          }}
+        >
+          <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+            <Container
+              maxWidth="md"
+              sx={{ textAlign: "center", width: "auto", padding: "0" }}
+            >
+              <Typography variant="h2" component="h2">
+                Document Summary Generator
+              </Typography>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
   );
 }
 
